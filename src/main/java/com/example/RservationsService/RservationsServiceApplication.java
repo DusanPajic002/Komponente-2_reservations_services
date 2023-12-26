f708243c16c0b7fb7d3604d95ed70599872a46f4
DusanPajic002 <141676628+DusanPajic002@users.noreply.github.com>
2023-12-26 13:05:05 +0100

    Add files via upload

---
+++ src/main/java/com/example/RservationsService/RservationsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.RservationsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RservationsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RservationsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RservationsService/RservationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RservationsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RservationsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
